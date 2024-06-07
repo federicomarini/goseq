0e3ce667da98227056ffa87b285b61b434f2be95
Federico Marini <marinif@uni-mainz.de>
2024-06-08 01:06:30 +0200

    replacing F with a more descriptive Fmonocon, avoids misuse with F/FALSE

--- R/makespline.R
+++ R/makespline.R
@@ -36,14 +36,14 @@ makespline=function (x, y, newX=NULL, nKnots = 6, lower_bound=10^-3){
 	sm <- smoothCon(s(x, k = nKnots, bs = "cr"), dat, knots = NULL)[[1]]
 	if (length(sm$xp) < 6) 
 		warning("Few than 6 nKnots were specified!\n Possible inaccurate fitting!\n")
-	F <- mono.con(sm$xp,TRUE)
+	Fmonocon <- mono.con(sm$xp,TRUE)
 	# The lower and upper parameters here don't seem to build the right constraints 100% of the time, so add them manually
 	# ND-6/14- removing the upper limit constraint because it wasn't satisfied by initial parameters.
 	# Hope it doesn't break the code.
-	F$A=rbind(F$A,c(1,rep(0,ncol(F$A)-1))) #,c(rep(0,ncol(F$A)-1)),-1))
-	F$b=c(F$b,0) #,1) ND - should this last one be -1?
+	Fmonocon$A=rbind(Fmonocon$A,c(1,rep(0,ncol(Fmonocon$A)-1))) #,c(rep(0,ncol(F$A)-1)),-1))
+	Fmonocon$b=c(Fmonocon$b,0) #,1) ND - should this last one be -1?
 	G <- list(X = sm$X, C = matrix(0, 0, 0), sp = f.ug$sp, p = sm$xp,
-	    y = y, w = y * 0 + 1, Ain =F$A, bin = F$b, S = sm$S, off = 0)
+	    y = y, w = y * 0 + 1, Ain =Fmonocon$A, bin = Fmonocon$b, S = sm$S, off = 0)
 	# do some error checking as pcls can return NaNs sometimes if this isn't true
 	if( any((G$Ain%*%G$p - G$bin) < 0 )){
 	   show(G$p)
